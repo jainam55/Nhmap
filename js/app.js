5a25dc6d8323abb438b9a33151969851531ff323
Jainam Shah <jainam55@yahoo.co.in>
2018-07-23 12:31:35 -0400

    Deleted unwanted

--- js/app.js
+++ js/app.js
@@ -145,8 +145,8 @@ var ViewModel = function(){
 
 function makeMarkerIcon(markerColor) {
         this.markerImage = new google.maps.MarkerImage(
-          'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
-          '|40|_|%E2%80%A2',
+          "http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|"+ markerColor +
+          "|40|_|%E2%80%A2",
           new google.maps.Size(21, 34),
           new google.maps.Point(0, 0),
           new google.maps.Point(10, 34),
